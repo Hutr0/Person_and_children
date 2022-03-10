42bceb30e69f5433bfd441fc9e17a749224bd8dd
Hutr0 <Hutro-games@yandex.ru>
2022-03-10 18:27:16 +0300

    Initial Commit

---
+++ PersonAndChildren/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonAndChildren
+//
+//  Created by Леонид Лукашевич on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
